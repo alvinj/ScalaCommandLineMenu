e36e670be19e9f1f36908d4cb4911efbc6b0173b
Alvin Alexander <devdaily@gmail.com>
2012-08-13 11:54:35 -0600

    init

---
+++ src/test/scala/com/alvinalexander/menu/MenuRunnerTests.scala
@@ -0,0 +1,11 @@
+package com.alvinalexander.menu
+
+import org.scalatest.FunSuite
+
+class MenuRunnerTests extends FunSuite {
+  
+  test("a simple fake test") {
+    assert("foo" == "bar")
+  }
+
+}
